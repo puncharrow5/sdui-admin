388b9b3bde6a8593f72748d8c82b9d203a14abf3
puncharrow5 <osh4761@hanmail.net>
2024-06-19 18:40:57 +0900

    feat: 섹션 컴포넌트

---
+++ components/Section/Section.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ComponentEntity } from "@/graphql/generated/types";
+import * as S from "./Section.style";
+
+interface Props {
+  id: string;
+  data: ComponentEntity;
+}
+
+export const Section = ({ data, id }: Props) => {
+  return (
+    <div
+      id={id}
+      className={`flex flex-col justify-center items-center text-center w-full min-h-[754px] h-full`}
+      style={{ backgroundColor: data.background ?? "#FFF" }}
+    >
+      <S.Title titleStyle={data.titleStyle ?? undefined}>{data.title}</S.Title>
+      <S.Content
+        contentStyle={data.contentStyle ?? undefined}
+        dangerouslySetInnerHTML={{ __html: data.content as TrustedHTML }}
+      />
+    </div>
+  );
+};
